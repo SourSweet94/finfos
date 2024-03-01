84a1e8706ef68ed05ff417bd20b0cd980da524f1
Chee Yung Fam <cheeyung12@gmail.com>
2024-03-02 00:22:23 +0800

    - Menu.tsx - add on weekDates selection to display spicific week's food - Amend Home and Sidebar styling

--- frontend/src/pages/Menu.tsx
+++ frontend/src/pages/Menu.tsx
@@ -1,18 +1,18 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { FoodContext } from "../context/FoodContext";
 import { AuthContext } from "../context/AuthContext";
 import FoodCard from "../components/FoodCard";
-import { Col, Container, Row } from "react-bootstrap";
+import { ButtonGroup, Col, Container, Dropdown, Row } from "react-bootstrap";
 
 const Menu = () => {
   const {
     state: { food },
     dispatch,
   } = useContext(FoodContext);
-  // const { record_id, food_id } = useContext(ItemContext);
   const {
     state: { user },
   } = useContext(AuthContext);
+  const [selectedWeek, setSelectedWeek] = useState<number>(0);
 
   useEffect(() => {
     const fetchFood = async () => {
@@ -23,28 +23,77 @@ const Menu = () => {
       });
       const json = await response.json();
 
+      const filteredJson = json.filter((item: any) => {
+        const itemWeek = getISOWeek(new Date(item.date));
+        return selectedWeek === 0 || itemWeek === selectedWeek;
+      });
+      const selectedDates = generateWeekDates(selectedWeek);
+      console.log(selectedDates);
       if (response.ok) {
-        dispatch({ type: "SET_FOOD", payload: json });
+        dispatch({ type: "SET_FOOD", payload: filteredJson });
       }
     };
     if (user) {
       fetchFood();
     }
-  }, [dispatch, user]);
+  }, [dispatch, user, selectedWeek]);
 
-  console.log(food);
+  const getISOWeek = (date: Date) => {
+    const firstDayOfYear = new Date(date.getFullYear(), 0, 1);
+    const days = Math.round(
+      (date.getTime() - firstDayOfYear.getTime()) / (24 * 3600 * 1000)
+    );
+    const isoWeek = Math.ceil((days + firstDayOfYear.getDay() + 1) / 7);
+    return isoWeek;
+  };
 
+  const generateWeekDates = (week: number) => {
+    const currentDate = new Date();
+    const firstDayOfYear = new Date(currentDate.getFullYear(), 0, 1);
+    const firstDayOfWeek = new Date(firstDayOfYear);
+    firstDayOfWeek.setDate(
+      firstDayOfYear.getDate() + (week - 1) * 7 - firstDayOfYear.getDay()
+    );
+
+    const dates = Array.from({ length: 7 }, (_, index) => {
+      const date = new Date(firstDayOfWeek);
+      //from Sunday to Saturday
+      date.setDate(firstDayOfWeek.getDate() + index + 1);
+      return date.toISOString().substring(0, 10);
+    });
+
+    return dates;
+  };
+
+  const handleWeekSelect = (week: number) => {
+    setSelectedWeek(week);
+  };
   return (
     <>
       <Container>
-        <Row style={{display: 'flex'}}>
+      <Row>
+        <Col>
+          <Dropdown as={ButtonGroup }>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              {selectedWeek === 0 ? "All Weeks" : `Week ${selectedWeek}`}
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item onClick={() => handleWeekSelect(0)}>All Weeks</Dropdown.Item>
+              <Dropdown.Item onClick={() => handleWeekSelect(7)}>Week 7</Dropdown.Item>
+              <Dropdown.Item onClick={() => handleWeekSelect(8)}>Week 8</Dropdown.Item>
+              <Dropdown.Item onClick={() => handleWeekSelect(9)}>Week 9</Dropdown.Item>
+              <Dropdown.Item onClick={() => handleWeekSelect(10)}>Week 10</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Col>
+      </Row>
+        <Row style={{ display: "flex" }}>
           {food?.map((food) => {
-          return (
-            <FoodCard key={food._id} title={food.title} price={food.price} />
-          );
-        })}
+            return (
+              <FoodCard key={food._id} title={food.title} price={food.price} />
+            );
+          })}
         </Row>
-        
       </Container>
     </>
   );
